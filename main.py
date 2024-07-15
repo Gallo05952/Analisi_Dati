780f0537219abed68f9eb1d6c829e127c684fc80
Gallo05952 <136304229+Gallo05952@users.noreply.github.com>
2024-07-15 17:14:42 +0200

    eseguibili V1

--- main.py
+++ main.py
@@ -197,35 +197,35 @@ def App(finesta_principale):
                         text="")
     empty_row.grid(row=9, column=0)
 
-    # BOTTONE GRAFICI VARIABILI
-    bottone_grafici = tk.Button(finesta_principale,
-                            text="Grafici",
-                            command=lambda: Grafici_Variabili(),
-                            bg="light grey",
-                            font=("Arial", 12),
-                            fg="black")
-    bottone_grafici.config(state=tk.DISABLED)
-    bottone_grafici.grid(row=4, column=2)
+    # # BOTTONE GRAFICI VARIABILI
+    # bottone_grafici = tk.Button(finesta_principale,
+    #                         text="Grafici",
+    #                         command=lambda: Grafici_Variabili(),
+    #                         bg="light grey",
+    #                         font=("Arial", 12),
+    #                         fg="black")
+    # bottone_grafici.config(state=tk.DISABLED)
+    # bottone_grafici.grid(row=4, column=2)
 
-    # BOTTONE GRAFICI DENSITà DI PROBABILITà
-    bottone_grafici_stat = tk.Button(finesta_principale,
-                                text="Grafici Probabilità",
-                                command=lambda: Grafici_Probabilità(),
-                                bg="light grey",
-                                font=("Arial", 12),
-                                fg="black")
-    bottone_grafici_stat.config(state=tk.DISABLED)
-    bottone_grafici_stat.grid(row=6, column=2)
+    # # BOTTONE GRAFICI DENSITà DI PROBABILITà
+    # bottone_grafici_stat = tk.Button(finesta_principale,
+    #                             text="Grafici Probabilità",
+    #                             command=lambda: Grafici_Probabilità(),
+    #                             bg="light grey",
+    #                             font=("Arial", 12),
+    #                             fg="black")
+    # bottone_grafici_stat.config(state=tk.DISABLED)
+    # bottone_grafici_stat.grid(row=6, column=2)
 
-    # BOTTONE GRAFICI CORRELAZIONE
-    bottone_grafici_corr = tk.Button(finesta_principale,
-                                text="Grafici Correlazione",
-                                command=lambda: Grafici_Correlazione(),
-                                bg="light grey",
-                                font=("Arial", 12),
-                                fg="black")
-    bottone_grafici_corr.config(state=tk.DISABLED)
-    bottone_grafici_corr.grid(row=8, column=2)
+    # # BOTTONE GRAFICI CORRELAZIONE
+    # bottone_grafici_corr = tk.Button(finesta_principale,
+    #                             text="Grafici Correlazione",
+    #                             command=lambda: Grafici_Correlazione(),
+    #                             bg="light grey",
+    #                             font=("Arial", 12),
+    #                             fg="black")
+    # bottone_grafici_corr.config(state=tk.DISABLED)
+    # bottone_grafici_corr.grid(row=8, column=2)
 
     # BOTTONE SALVA
     bottone_salva = tk.Button(finesta_principale,
